7c266c83c955f08fa49129e11e358e0e8b7cb765
taoxinyyyun <tesstao0428@gmail.com>
2023-03-18 01:17:13 -0700

    PALs with encoder + gradient surgery

--- multitask_classifier.py
+++ multitask_classifier.py
@@ -63,14 +63,14 @@ class MultitaskBERT(nn.Module):
         self.cos = nn.CosineSimilarity(dim=1, eps=1e-6)
 
 
-    def forward(self, input_ids, attention_mask):
+    def forward(self, input_ids, attention_mask, task_id=0):
         'Takes a batch of sentences and produces embeddings for them.'
         # The final BERT embedding is the hidden state of [CLS] token (the first token)
         # Here, you can start by just returning the embeddings straight from BERT.
         # When thinking of improvements, you can later try modifying this
         # (e.g., by adding other layers).
         ### TODO
-        pooled_output = self.bert(input_ids=input_ids, attention_mask=attention_mask)['pooler_output']
+        pooled_output = self.bert(input_ids=input_ids, attention_mask=attention_mask, task_id=task_id)['pooler_output']
         return pooled_output
         
 
@@ -82,7 +82,7 @@ class MultitaskBERT(nn.Module):
         Thus, your output should contain 5 logits for each sentence.
         '''
         ### TODO
-        pooled_output = self.forward(input_ids, attention_mask)
+        pooled_output = self.forward(input_ids, attention_mask, 0)
         pooled_output = self.dropout(pooled_output)
         out = self.linear_sent(pooled_output)
         return out
@@ -96,10 +96,10 @@ class MultitaskBERT(nn.Module):
         during evaluation, and handled as a logit by the appropriate loss function.
         '''
         ### TODO
-        pooled_output1 = self.forward(input_ids_1, attention_mask_1)
+        pooled_output1 = self.forward(input_ids_1, attention_mask_1, 1)
         pooled_output1 = self.dropout(pooled_output1)
         pooled_output1 = self.linear_para1(pooled_output1)
-        pooled_output2 = self.forward(input_ids_2, attention_mask_2)
+        pooled_output2 = self.forward(input_ids_2, attention_mask_2, 1)
         pooled_output2 = self.dropout(pooled_output2)
         pooled_output2 = self.linear_para2(pooled_output2)
         cos_dist = torch.diagonal(torch.mm(pooled_output1, pooled_output2.t()))
@@ -116,10 +116,10 @@ class MultitaskBERT(nn.Module):
         '''
         # Update: it is no longer passed to sigmoid in evaluation.py
         ### TODO
-        pooled_output1 = self.forward(input_ids_1, attention_mask_1)
+        pooled_output1 = self.forward(input_ids_1, attention_mask_1, 2)
         pooled_output1 = self.dropout(pooled_output1)
         pooled_output1 = self.linear_sim1(pooled_output1)
-        pooled_output2 = self.forward(input_ids_2, attention_mask_2)
+        pooled_output2 = self.forward(input_ids_2, attention_mask_2, 2)
         pooled_output2 = self.dropout(pooled_output2)
         pooled_output2 = self.linear_sim2(pooled_output2)
         cos_dist = torch.diagonal(torch.mm(pooled_output1, pooled_output2.t()))
