6dbf00588f69396f0b2e7e69402223814fcbb823
taoxinyyyun <tesstao0428@gmail.com>
2023-03-18 00:42:59 -0700

    added pal and pal config

--- bert.py
+++ bert.py
@@ -6,6 +6,7 @@ import torch.nn.functional as F
 from base_bert import BertPreTrainedModel
 from utils import *
 
+NUM_TASKS = 3
 
 class BertPals(nn.Module):
     def __init__(self, config):
@@ -14,7 +15,7 @@ class BertPals(nn.Module):
         #Encoder and decoder matrics project down to the smaller dimension
         self.aug_dense = nn.Linear(config.hidden_size, config.hidden_size_aug)
         self.aug_dense2 = nn.Linear(config.hidden_size_aug, config.hidden_size)
-        self.attn = BertSelfAttention(config,6)
+        self.attn = BertSelfAttentionPAL(config)
         self.config = config
         self.hidden_act_fn = F.gelu
         
@@ -137,6 +138,98 @@ class BertSelfAttention(nn.Module):
     attn_value = self.attention(key_layer, query_layer, value_layer, attention_mask)
     return attn_value
 
+class BertSelfAttentionPAL(nn.Module):
+  def __init__(self, config):
+    super().__init__()
+
+    self.num_attention_heads = config.num_attention_heads
+    self.attention_head_size = int(config.hidden_size_aug / config.num_attention_heads)
+    self.all_head_size = self.num_attention_heads * self.attention_head_size
+
+    # initialize the linear transformation layers for key, value, query
+    self.query = nn.Linear(config.hidden_size_aug, self.all_head_size)
+    self.key = nn.Linear(config.hidden_size_aug, self.all_head_size)
+    self.value = nn.Linear(config.hidden_size_aug, self.all_head_size)
+    # this dropout is applied to normalized attention scores following the original implementation of transformer
+    # although it is a bit unusual, we empirically observe that it yields better performance
+    self.dropout = nn.Dropout(config.attention_probs_dropout_prob)
+
+  def transform(self, x, linear_layer):
+    # the corresponding linear_layer of k, v, q are used to project the hidden_state (x)
+    bs, seq_len = x.shape[:2]
+    proj = linear_layer(x)
+    # next, we need to produce multiple heads for the proj 
+    # this is done by spliting the hidden state to self.num_attention_heads, each of size self.attention_head_size
+    proj = proj.view(bs, seq_len, self.num_attention_heads, self.attention_head_size)
+    # by proper transpose, we have proj of [bs, num_attention_heads, seq_len, attention_head_size]
+    proj = proj.transpose(1, 2)
+    return proj
+
+  def attention(self, key, query, value, attention_mask):
+    # each attention is calculated following eq (1) of https://arxiv.org/pdf/1706.03762.pdf
+    # attention scores are calculated by multiply query and key 
+    # and get back a score matrix S of [bs, num_attention_heads, seq_len, seq_len]
+    # S[*, i, j, k] represents the (unnormalized)attention score between the j-th and k-th token, given by i-th attention head
+    # before normalizing the scores, use the attention mask to mask out the padding token scores
+    # Note again: in the attention_mask non-padding tokens with 0 and padding tokens with a large negative number 
+
+    # normalize the scores
+    # multiply the attention scores to the value and get back V'
+    # next, we need to concat multi-heads and recover the original shape [bs, seq_len, num_attention_heads * attention_head_size = hidden_size]
+
+    ### TODO
+    
+    #each k, q, v is projected to size [bs, num_attention_heads, seq_len, attention_head_size]
+    bs, dummy, seq_len = key.shape[:3]
+    
+    #K^T
+    key = key.transpose(2, 3)
+    
+    #QK^T
+    score = query@key
+    
+    #dividing scaling factor, we get a score matrix S of [bs, num_attention_heads, seq_len, seq_len]
+    score /= (self.attention_head_size**0.5)
+    
+    # attention mask to mask out the padding token scores
+    score += attention_mask
+    
+    #applying softmax: input is of shape (s1, s2, s3, s4) then dim of softmax is 3
+    sm = nn.Softmax(dim=3)
+    score = sm(score)
+    
+    #TODO: do we need dropout
+    #dropout applied to normalized attention scores
+    score = self.dropout(score)
+    
+    # multiply the attention scores to the value and get back V'
+    V_prime = score@value #[bs, num_attention_heads, seq_len, attention_head_size]
+    
+    #transpose V' to shape [bs, seq_len, num_attention_heads, attention_head_size]
+    #get a contiguous copy after transpose
+    V_prime = V_prime.transpose(1, 2).contiguous()
+    
+    #concat multi-heads and recover the original shape [bs, seq_len, num_attention_heads * attention_head_size = hidden_size]
+    oup = V_prime.view(bs, seq_len, self.all_head_size) #[bs, seq_len, num_attention_heads * attention_head_size]
+    
+    return oup
+    
+
+  def forward(self, hidden_states, attention_mask):
+    """
+    hidden_states: [bs, seq_len, hidden_state]
+    attention_mask: [bs, 1, 1, seq_len]
+    output: [bs, seq_len, hidden_state]
+    """
+    # first, we have to generate the key, value, query for each token for multi-head attention w/ transform (more details inside the function)
+    # of *_layers are of [bs, num_attention_heads, seq_len, attention_head_size]
+    key_layer = self.transform(hidden_states, self.key)
+    value_layer = self.transform(hidden_states, self.value)
+    query_layer = self.transform(hidden_states, self.query)
+    # calculate the multi-head attention 
+    attn_value = self.attention(key_layer, query_layer, value_layer, attention_mask)
+    return attn_value
+
 
 class BertLayer(nn.Module):
   def __init__(self, config):
@@ -154,6 +247,16 @@ class BertLayer(nn.Module):
     self.out_dense = nn.Linear(config.intermediate_size, config.hidden_size)
     self.out_layer_norm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
     self.out_dropout = nn.Dropout(config.hidden_dropout_prob)
+
+    self.config = config
+    # pal and lowrank extension
+    multi = None
+    self.multi_layers = None
+    if config.extension_option == 'pal':
+      multi = BertPals(config)
+      # elif config.extension_option == 'lowrank':
+      #   multi = BERTLowRank(config)
+    self.multi_layers = nn.ModuleList([copy.deepcopy(multi) for _ in range(NUM_TASKS)])
 
   def add_norm(self, input, output, dense_layer, dropout, ln_layer):
     """
@@ -179,7 +282,7 @@ class BertLayer(nn.Module):
     return output
     
 
-  def forward(self, hidden_states, attention_mask):
+  def forward(self, hidden_states, attention_mask, task_id=0):
     """
     hidden_states: either from the embedding layer (first bert layer) or from the previous bert layer
     as shown in the left of Figure 1 of https://arxiv.org/pdf/1706.03762.pdf 
@@ -202,7 +305,12 @@ class BertLayer(nn.Module):
     oup3 = self.interm_af(oup3)
     
     #a add-norm that takes the input and output of the feed forward layer
-    oup4 = self.add_norm(oup2, oup3, self.out_dense, self.out_dropout, self.out_layer_norm)
+    if self.config.extension_option == 'none':
+      oup4 = self.add_norm(oup2, oup3, self.out_dense, self.out_dropout, self.out_layer_norm)
+    else:
+      # print('extension', config.extension_option)
+      extra = self.multi_layers[task_id](hidden_states, attention_mask)
+      oup4 = self.add_norm(oup2 + extra, oup3, self.out_dense, self.out_dropout, self.out_layer_norm)
     
     return oup4
 
@@ -232,6 +340,19 @@ class BertModel(BertPreTrainedModel):
 
     # bert encoder
     self.bert_layers = nn.ModuleList([BertLayer(config) for _ in range(config.num_hidden_layers)])
+
+    #pal encoder
+    #only for PALs
+    if config.extension_option == 'pal':
+        dense = nn.Linear(config.hidden_size, config.hidden_size_aug)
+        # Shared encoder and decoder across layers
+        self.mult_aug_dense = nn.ModuleList([copy.deepcopy(dense) for _ in range(NUM_TASKS)])
+        dense2 = nn.Linear(config.hidden_size_aug, config.hidden_size)
+        self.mult_aug_dense2 = nn.ModuleList([copy.deepcopy(dense2) for _ in range(NUM_TASKS)])
+        for l, layer in enumerate(self.layer):
+            for i, lay in enumerate(layer.multi_layers):
+                lay.aug_dense = self.mult_aug_dense[i]
+                lay.aug_dense2 = self.mult_aug_dense2[i]
 
     # for [CLS] token
     self.pooler_dense = nn.Linear(config.hidden_size, config.hidden_size)
